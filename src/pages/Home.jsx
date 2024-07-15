619f2e9bcb3ee318bd70b1aced11c4dc3f1c3081
yongqi8899 <1243994415@qq.com>
2024-07-15 15:53:59 +0200

    add logout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
 export default function Home() {
-    return(
-        <div>Home</div>
-    )
+    return (
+        <>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="text-center hero-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">
+                            Welcome to the Home Page
+                        </h1>
+                        <p className="mb-5">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus, non, dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum! Quasi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
